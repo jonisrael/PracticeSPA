a1e82aa9807c82859f361ad16559627363cd6d61
Jonathan Israel <jonisrael45@gmail.com>
2021-08-17 21:07:12 -0500

    Changes from 9.2 Tuesday Class

--- server/index.js
+++ server/index.js
@@ -1,14 +1,57 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const greetings = require("./routers/greetings.js");
+const pizzas = require("./routers/pizzas.js");
+
+dotenv.config();
 
 // Import ^^^^
-// Instansiation of variables
+// Instantiation of variables
 const app = express();
 
-// Configuring Express instance
+mongoose.connect(process.env.MONGODB);
+const db = mongoose.connection;
 
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
+// Configuring Express instance
 app.get("/status", (request, response) => {
   response.send(JSON.stringify({ message: "Service healthy" }));
 });
+
+// Defining the middleware functions
+const logging = (request, response, next) => {
+  console.log(`${request.method} ${request.url} ${Date.now()}`);
+  next();
+};
+
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*"); // using * is "promiscuous", don't do it
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+
+// Using the middleware functions
+app.use(cors);
+app.use(express.json());
+app.use(logging);
+app.use(greetings);
+app.use(pizzas);
 
 app
   .route("/")
@@ -25,10 +68,11 @@ app
     );
   });
 
-app.route("/greet/:name").get((request, response) => {
+app.route("/greetings/:name").get((request, response) => {
   const name = request.params.name;
   response.status(418).json({ message: `Hello ${name}` });
 });
 
 // Executing the Express (This must be last)
-app.listen(4040, () => console.log("Listening on port 4040"));
+const port = process.env.PORT || 4040;
+app.listen(4040, () => console.log(`Listening on port ${port}`));
